e8f28b8858be6ccea2ff7c7b616291cb28d2fce2
Sebastian <smillerr33@gmail.com>
2023-08-04 20:35:56 -0500

    Initial view of the project

---
+++ constants.example.js
@@ -0,0 +1,6 @@
+export const navigationLandingPage = [
+    { name: 'Some link', href: '#' },
+    { name: 'Another link', href: '#' },
+    { name: 'Link #3', href: '#' },
+    { name: 'Last link', href: '#' },
+]; 

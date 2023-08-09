3f2ab608f42eca110a66d1a902cf562782ecbc87
Sebastian Miller <smillerr33@gmail.com>
2023-08-09 12:06:33 -0500

    [ADD] Common components

--- constants.example.js
+++ constants.example.js
@@ -4,3 +4,13 @@ export const navigationLandingPage = [
     { name: 'Link #3', href: '#' },
     { name: 'Last link', href: '#' },
 ]; 
+export const staffMembers = [
+    {
+      name: 'John Doe',
+      title: 'Your fabolous title',
+      department: 'Some department',
+      role: 'Super importan role',
+      email: 'john.doe@example.com',
+      image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+]

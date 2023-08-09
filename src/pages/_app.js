fd07a9554e2891f584dd800f91651b453a289060
Sebastian Miller <smillerr33@gmail.com>
2023-08-09 14:08:15 -0500

    [ADD] App layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
+import MainLayout from '@layout/MainLayout'
 import '@styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>  
+    </>
+  )  
 }
 
 export default MyApp

6baf97c302af1129324cbae09ffb1b524225f813
Sebastian Miller <smillerr33@gmail.com>
2023-08-09 17:05:41 -0500

    [ADD] Fake Store API endpoints

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,14 @@
+import React, { useRef } from 'react'
 import { LockClosedIcon } from '@heroicons/react/solid'
 
-export default function Login() {
+export default function LoginForm() {
+  const formRef = useRef()
+  const handleSubmitForm = (e) => {
+    e.preventDefault()
+    const formFields = new FormData(formRef.current)
+    const emailField = formFields.get('email')
+    const pwdField = formFields.get('password')
+  }
   return (
     <>
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -9,7 +17,7 @@ export default function Login() {
             <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
           </div>
-          <form className="mt-8 space-y-6">
+          <form className="mt-8 space-y-6" ref={formRef} onSubmit={handleSubmitForm}>
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
---
+++ src/services/storeAPI/endpoints.js
@@ -0,0 +1,25 @@
+const BASE_API_URL = process.env.STORE_PUBLIC_API_URL
+const API_VERSION = process.env.STORE_PUBLIC_API_VERSION
+
+const endpoints = {
+    products: {
+        getProducts: `${BASE_API_URL}${API_VERSION}/products`,
+        getProduct: (id)=>`${BASE_API_URL}${API_VERSION}/products/${id}`,
+        createProduct: `${BASE_API_URL}${API_VERSION}/products`,
+        updateProduct: (id)=>`${BASE_API_URL}${API_VERSION}/products/${id}`,
+        deleteProduct: (id)=>`${BASE_API_URL}${API_VERSION}/products/${id}`,
+    },
+    auth: {
+        login:  `${BASE_API_URL}${API_VERSION}/auth/login`,
+        profile: `${BASE_API_URL}${API_VERSION}/auth/profile`,
+        refreshToken: `${BASE_API_URL}${API_VERSION}/auth/refresh-token`,
+    },
+    users: {
+        getUsers: `${BASE_API_URL}${API_VERSION}/users`,
+        getUser: (id) => `${BASE_API_URL}${API_VERSION}/users/${id}`,
+        createUser: `${BASE_API_URL}${API_VERSION}/users`,
+        updateUser: (id)=>`${BASE_API_URL}${API_VERSION}/users/${id}`,
+        deleteUser: (id)=>`${BASE_API_URL}${API_VERSION}/users/${id}`,
+        checkUser: `${BASE_API_URL}${API_VERSION}/users/is-available`,
+    }
+}
